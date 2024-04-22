d89fcdad10086db083f85f693bdea774a18334fd
AralCA <aralcavlak@gmail.com>
2024-04-22 20:28:57 +0300

    Intro to the front end.

--- Test.java
+++ Test.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import Parser.Parser;
 public class Test {
     public static void main(String[] args) {
-        try {
+        try { //It's apperant that the code was NOT written by the Communist of the group,
+                //Because it separates the lines.
             FileReader fr = new FileReader("test.txt");
             Parser p = new Parser(fr);
             p.workflowTokenizer();
@@ -17,5 +18,54 @@ public class Test {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+
+
+    }
+}
+
+class Station{
+    private String name;
+    private boolean multiFlag;
+    private boolean fifoFlag;
+
+    Station(String name, boolean multiFlag, boolean fifoFlag){
+        this.name = name;
+        this.multiFlag = multiFlag;
+        this.fifoFlag = fifoFlag;
+    }
+}
+
+class Job{
+    private String name;
+    private JobType type;
+    private float startTime;
+    private float duration;
+    
+    Job(String name, JobType type, float startTime, float duration){
+        this.name = name;
+        this.type = type;
+        this.startTime = startTime;
+        this.duration = duration;
+    }
+}
+
+class JobType{
+    private ArrayList<Task> tasks;
+    private String name;
+
+    JobType(ArrayList<Task> tasks, String name){
+        this.tasks = tasks;
+        this.name = name;
+    }
+}
+
+class Task{
+    private String name;
+    private float size;
+
+    Task(String name, float size){
+        this.name = name;
+        this.size = size;
     }
 }
