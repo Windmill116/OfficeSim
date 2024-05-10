00135b88c98de404db67348fef80414a0ec90d3c
Aykan Uğur <127043240+aykanugur@users.noreply.github.com>
2024-05-10 13:52:12 +0300

    clean code!!!!!!

--- src/Test.java
+++ src/Test.java
@@ -20,7 +20,7 @@ public class Test {
             ArrayList<String> jobTokens =  p.getJobTokens();
             
             Organizer organizer = new Organizer(tokens, p.getLine(), jobTokens);
-           
+            
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
