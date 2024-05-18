e2a483996592916f1e215d1a5a4ca2ca65b16891
AralCA <aralcavlak@gmail.com>
2024-05-18 10:52:30 +0300

    Job Tardiness has been added

--- src/Parser/Job.java
+++ src/Parser/Job.java
@@ -1,4 +1,5 @@
 package Parser;
+import Frontend.EventTemplate;
 import java.util.ArrayList;
 
 public class Job {
@@ -7,7 +8,11 @@ public class Job {
     private float startTime;
     private float duration;
     private String jobId;
+    private float jobTardiness = 0;
+    
+
     ArrayList<Task> tasks = new ArrayList<Task>();
+    ArrayList<EventTemplate> eventTemplates = new ArrayList<EventTemplate>();
 
     public Job(String jobId,JobType jobType, float startTime, float duration) {
         this.jobId = jobId;
@@ -55,5 +60,31 @@ public class Job {
 
     public ArrayList<Task> getTasks(){
         return tasks;
+    }
+
+    public ArrayList<EventTemplate> getEventTemplates() {
+        return eventTemplates;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void setEventTemplates(ArrayList<EventTemplate> eventTemplates) {
+        this.eventTemplates = eventTemplates;
+    }
+
+    public void calculateJobTardiness(){
+        float jobFinishTime = getDuration() + getStartTime();
+        float lastEventTime = getEventTemplates().getLast().getTime();
+        jobTardiness = lastEventTime - jobFinishTime;
+    }
+
+    public float getJobTardiness() {
+        return jobTardiness;
+    }
+
+    public void setJobTardiness(float jobTardiness) {
+        this.jobTardiness = jobTardiness;
     }
 }
